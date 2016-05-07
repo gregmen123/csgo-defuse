18cbcdfa5faad204cabfc5f45650408b35bc5c03
Kevin Menhinick <kamenhinick@gmail.com>
2016-05-07 18:56:45 +1200

    Fixed Kev's silly mistakes

--- Main.ino
+++ Main.ino
@@ -1,12 +1,12 @@
-int[] ledPins = [13, 12, 11, 10, 9];
+int ledPins[] = {13, 12, 11, 10, 9};
 //  represents:  r1  r2  a1  a2 g1
 // how to use: instead of pinMode(red1, OUTPUT) (for example), use pinMode(ledPins[0])
 int defusePin = 0;
-int delayTime = 1000;
 int currentStage = 0;
+int delayTime = 1000;
 
 void setup() {
-  pinMode(defuse, INPUT);
+  pinMode(defusePin, INPUT);
   pinMode(ledPins[0], OUTPUT);
   pinMode(ledPins[1], OUTPUT);
   pinMode(ledPins[2], OUTPUT);
@@ -16,20 +16,20 @@ void setup() {
 
 void loop() {
 
-  if(digitalRead(defuse) == HIGH) {
-    if(currentStage < 5) {
-      setProgress(currentStage ++);
+  if(digitalRead(defusePin) == HIGH) {
+    if(currentStage < 5) { // If the lights aren't fully lit up yet...
+      setProgress(currentStage ++); // Set the lights up to the current stage, and add one to currnet stage
       delay(delayTime);
     }
     else {
-      flashAll(5, delayTime / 2);
+      flashAll(5, delayTime / 2); // Flash all 5 times, using only half our delayTime variable
     }
   }
   else {
-    currentStage = 0;
+    currentStage = 0; // The cable was disconnected, so set currentStage back to zero and turn off lights:
     setProgress(0);
   }
-  
+
 }
 
 void flashAll(int times, int _delayTime) {
